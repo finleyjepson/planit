97370159d347a23a830836b463113842c7052cdb
Finley <108339048+finleyjepson@users.noreply.github.com>
2022-07-15 12:39:59 +1000

    added notes

--- highest occur char.py
+++ highest occur char.py
@@ -1,3 +1,4 @@
+# finds the most occurring character in the string
 string1="iHateuLOL".lower()
 list1=[]
 list2=[]
@@ -5,6 +6,10 @@ for i in string1:
   if i not in list1:
     list1.append(i)
     list2.append(string1.count(i))
+# finds max in the list
 occ=max(list2)
 ele=list1[list2.index(occ)]
+# prints the string plus highest occuring character
 print("in", string1, "the character", ele, "is the most occurring character")
+
+# contributed by Finley Jepson
